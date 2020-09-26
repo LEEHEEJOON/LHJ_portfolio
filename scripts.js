ec50ce6395b5b3a4dc2fb7c9c98252e3fadcfa71
LEEHEEJOON <marco920322@gmail.com>
2020-09-26 22:26:02 +0900

    반응형 웹사이트 변경 및 햄버거 메뉴 추가

--- scripts.js
+++ scripts.js
@@ -1,14 +1,24 @@
-$(document).ready(() => {
-  $(window).scroll(() => {
-    if (this.scrollY > 20) {
-      $('.navbar').addClass('sticky');
-    } else {
-      $('.navbar').removeClass('sticky');
-    }
-  });
+// $(document).ready(() => {
+//   $(window).scroll(() => {
+//     if (this.scrollY > 20) {
+//       $('.navbar').addClass('sticky');
+//     } else {
+//       $('.navbar').removeClass('sticky');
+//     }
+//   });
 
-  $('.menu-toggler').click(() => {
-    $(this).toggleClass('active');
-    $('.navbar-menu').toggleClass('active');
-  });
+//   $('.menu-toggler').click(() => {
+//     $(this).toggleClass('active');
+//     $('.navbar-menu').toggleClass('active');
+//   });
+// });
+
+const hamburger = document.querySelector(
+  '.header .nav-bar .nav-list .hamburger'
+);
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
 });
