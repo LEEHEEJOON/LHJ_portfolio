07f036d9b3829df850a29a0b69142137d2f633f7
LEEHEEJOON <marco920322@gmail.com>
2020-10-16 19:03:29 +0900

    Modify navbar and home

--- scripts.js
+++ scripts.js
@@ -1,28 +1,25 @@
-const navbar = document.querySelector('#navbar');
-const hamburger = document.querySelector(
-  '#navbar .container .nav-bar .nav-list .hamburger'
-);
-const mobile_menu = document.querySelector(
-  '#navbar .container .nav-bar .nav-list .navbar-menu'
-);
+const hamburger = document.querySelector('#navbar .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('#navbar .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('#navbar .nav-bar .nav-list ul li a');
+const navbar = document.querySelector('#navbar .container');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  mobile_menu.classList.toggle('active');
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
 });
 
 document.addEventListener('scroll', () => {
-  var scroll_position = window.scrollY;
-  if (scroll_position > 300) {
-    navbar.style.backgroundColor = '#29323c';
-  } else {
-    navbar.style.backgroundColor = 'transparent';
-  }
+	var scroll_position = window.scrollY;
+	if (scroll_position > 100) {
+		navbar.style.backgroundColor = 'rgb(31, 30, 30, 0.9)';
+	} else {
+		navbar.style.backgroundColor = 'transparent';
+	}
 });
 
-// menu_item.forEach((item) => {
-//   item.addEventListener('click', () => {
-//     hamburger.classList.toggle('active');
-//     mobile_menu.classList.toggle('active');
-//   });
-// });
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
